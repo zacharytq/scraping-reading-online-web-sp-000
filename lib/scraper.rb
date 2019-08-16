47c8a1d817ce178d645697221baa6ed991d85fd2
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 23:50:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
 
-
-
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-s60IBN")
